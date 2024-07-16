fcf956cec645494a3e0bcf86001553243b767ec4
MolchanovaOlga <designer.molchanova@gmail.com>
2024-07-17 01:48:04 +0300

    Add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router-dom";
+import Container from "./components/Container/Container";
+
 import "./App.css";
 
+const HomePage = lazy(() => import("./pages/HomePage"));
+const CatalogPage = lazy(() => import("./pages/CatalogPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
+
 function App() {
-  return <>Click on the Vite and React logos to learn more</>;
+  return (
+    <Container>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
