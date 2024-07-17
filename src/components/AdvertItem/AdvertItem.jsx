b612cdb4ed22905898046fbd67983a3a1b8e6db7
MolchanovaOlga <designer.molchanova@gmail.com>
2024-07-18 01:51:07 +0300

    Add sprite.svg

--- src/components/AdvertItem/AdvertItem.jsx
+++ src/components/AdvertItem/AdvertItem.jsx
@@ -1,4 +1,5 @@
 import css from "./AdvertItem.module.css";
+import sprite from "../../assets/sprite.svg";
 
 const AdvertItem = () => {
   return (
@@ -9,12 +10,26 @@ const AdvertItem = () => {
           <h2 className={css.title}>Mavericks</h2>
           <div className={css.priceContainer}>
             <p>€8000.00</p>
-            <button>icon</button>
+            <button className={css.heartBtn}>
+              <svg className={css.heartBtnIcon} width="24" height="24">
+                <use href={`${sprite}#icon-Heart`}></use>
+              </svg>
+            </button>
           </div>
         </div>
         <div className={css.revLocContainer}>
-          <p className={css.reviewsText}>4.4(2 Reviews)</p>
-          <p>Kyiv, Ukraine</p>
+          <div className={css.reviewsContainer}>
+            <svg className={css.starIcon} width="16" height="16">
+              <use href={`${sprite}#icon-Star`}></use>
+            </svg>
+            <p className={css.reviewsText}>4.4(2 Reviews)</p>
+          </div>
+          <div className={css.reviewsContainer}>
+            <svg className={css.mapPinIcon} width="16" height="16">
+              <use href={`${sprite}#icon-map-pin`}></use>
+            </svg>
+            <p>Kyiv, Ukraine</p>
+          </div>
         </div>
         <p className={css.description}>
           Embrace simplicity and freedom with the Mavericks panel truck, an
@@ -23,21 +38,39 @@ const AdvertItem = () => {
         </p>
         <ul className={css.detailsList}>
           <li className={css.detailsItem}>
+            <svg width="20" height="20">
+              <use href={`${sprite}#icon-Users`}></use>
+            </svg>
             <p>2 adults</p>
           </li>
           <li className={css.detailsItem}>
+            <svg className={css.detailIcon} width="20" height="20">
+              <use href={`${sprite}#icon-Transmission`}></use>
+            </svg>
             <p>Automatic</p>
           </li>
           <li className={css.detailsItem}>
+            <svg width="20" height="20">
+              <use href={`${sprite}#icon-Engine`}></use>
+            </svg>
             <p>Petrol</p>
           </li>
           <li className={css.detailsItem}>
+            <svg className={css.detailIcon} width="20" height="20">
+              <use href={`${sprite}#icon-Kitchen`}></use>
+            </svg>
             <p>Kitchen</p>
           </li>
           <li className={css.detailsItem}>
+            <svg className={css.detailIcon} width="20" height="20">
+              <use href={`${sprite}#icon-Bed`}></use>
+            </svg>
             <p>2 beds</p>
           </li>
           <li className={css.detailsItem}>
+            <svg width="20" height="20">
+              <use href={`${sprite}#icon-AC`}></use>
+            </svg>
             <p>AC</p>
           </li>
         </ul>
