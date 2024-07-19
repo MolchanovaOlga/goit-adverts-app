93a1a25ffdf64c625e3ad3688a9d42b2f6704880
MolchanovaOlga <designer.molchanova@gmail.com>
2024-07-20 02:49:10 +0300

    Add pagination to AdvertsList

--- src/components/AdvertsList/AdvertsList.jsx
+++ src/components/AdvertsList/AdvertsList.jsx
@@ -1,37 +1,53 @@
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import AdvertItem from '../AdvertItem/AdvertItem';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 import { selectAdvertsItems } from '../../redux/selectors';
 import css from './AdvertsList.module.css';
-
-import { useDispatch } from 'react-redux';
 import { getAdverts } from '../../redux/operations';
-import { useEffect } from 'react';
 
 const AdvertsList = () => {
   const dispatch = useDispatch();
+
+  const [perPage, setPerPage] = useState(4);
+  const [listAdvents, setListAdvents] = useState([]);
+  // const [loadMore, setLoadMore] = useState(false);
+
+  const adverts = useSelector(selectAdvertsItems);
 
   useEffect(() => {
     dispatch(getAdverts());
   }, [dispatch]);
 
-  const adverts = useSelector(selectAdvertsItems);
+  useEffect(() => {
+    setListAdvents(adverts.slice(0, perPage));
+  }, [adverts, perPage]);
+
+  const handleClick = () => {
+    setPerPage(prev => prev + 4);
+  };
 
   return (
-    <div className={css.listContainer}>
-      {adverts && adverts.length > 0 ? (
-        <ul className={css.list}>
-          {adverts.map(item => {
-            return (
-              <li className={css.item} key={item._id}>
-                <AdvertItem camperDetails={item} />
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        <p className={css.text}>There are no adverts in app yet.</p>
+    <div className={css.listAndLoadMoreContainer}>
+      <div className={css.listContainer}>
+        {listAdvents && listAdvents.length > 0 ? (
+          <ul className={css.list}>
+            {listAdvents.map(item => {
+              return (
+                <li className={css.item} key={item._id}>
+                  <AdvertItem camperDetails={item} />
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <p className={css.text}>There are no adverts in app yet.</p>
+        )}
+      </div>
+      {listAdvents.length < adverts.length && (
+        <LoadMoreBtn handleClick={handleClick} />
       )}
     </div>
   );
