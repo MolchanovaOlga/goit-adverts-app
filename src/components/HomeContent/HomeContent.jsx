845223dec9fb756eb4ff5433977f56075c08937f
MolchanovaOlga <designer.molchanova@gmail.com>
2024-07-21 20:04:36 +0300

    Add Home Page and styles

---
+++ src/components/HomeContent/HomeContent.jsx
@@ -0,0 +1,29 @@
+import css from './HomeContent.module.css';
+import carGif from '../../assets/car.gif';
+
+const HomeContent = () => {
+  return (
+    <div className={css.backgroundContainer}>
+      <p className={css.quot}>
+        “To Travel is to Live.” – Hans Christian Andersen
+      </p>
+      <div className={css.textContainer}>
+        <img src={carGif} alt="Car on the road" width="100" height="100" />
+        <div>
+          <h1 className={css.title}>Best Cars for Car Camping</h1>
+          <p className={css.mainText}>
+            Our company specializes in the provision of campervan rental
+            services for travel and recreation. We offer a wide range of modern
+            and reliable campers that provide maximum comfort and convenience
+            during travels.
+          </p>
+          <p className={css.text}>
+            Try it and your life will be filled with adventures.{' '}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeContent;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import DocumentTitle from '../components/DocumentTitle/DocumentTitle';
+import HomeContent from '../components/HomeContent/HomeContent';
 
 const HomePage = () => {
   return (
     <>
       <DocumentTitle>Adverts Home Page</DocumentTitle>
+      <HomeContent />
     </>
   );
 };
